39907d58addfa5b6ad9292c6936cf8bed5b00ad8
Leon Georgi <naytde@gmail.com>
2019-11-29 23:43:21 +0100

    Make Nicholas react on main key press

--- src/sprites/nicholas.py
+++ src/sprites/nicholas.py
@@ -1,6 +1,6 @@
 import pygame
 
-from context import Context
+from context import Context, Key
 from gamerect import GameRect
 from sprite import Sprite, Sprites
 
@@ -9,16 +9,23 @@ class Nicholas(Sprite):
 
     def __init__(self):
         self.__vpos = 0.5
-        self.__vspeed = -0.01
+        self.__vspeed = -0.005
+        self.__bounced = False
 
     def update(self, context: Context, sprites: Sprites):
         t = context.time_factor * 5
-        self.__vpos = 1 / 2 * context.gravity * (t ** 2) + \
+        a = context.gravity
+        if Key.MAIN in context.key_strokes and self.__vspeed >= 0:
+            a *= 2
+        self.__vpos = 1 / 2 * a * (t ** 2) + \
                       self.__vspeed * t + \
                       self.__vpos
-        self.__vspeed = context.gravity * t + self.__vspeed
-        if self.__vpos >= 0.9:
-            self.__vspeed = -abs(self.__vspeed)
+        self.__vspeed = a * t + self.__vspeed
+        if self.__vpos >= 0.9 and not self.__bounced:
+            self.__bounced = True
+            self.__vspeed = -abs(self.__vspeed) * 0.9
+        elif self.__vspeed < 0.9 and self.__bounced:
+            self.__bounced = False
 
     def render(self, surface: pygame.Surface, size_factor: float):
         surface.fill((0, 200, 0), self.box.to_pygame(size_factor))
