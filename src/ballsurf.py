e9eea24c81ef5932d4452f3ee35e6ecfe1128e2d
Leon Georgi <naytde@gmail.com>
2019-11-29 19:29:05 +0100

    Add pygame template

---
+++ src/ballsurf.py
@@ -0,0 +1,34 @@
+import sys
+
+import pygame
+from pygame.locals import *
+
+def run():
+    pygame.init()
+
+    fps = 60
+    fpsClock = pygame.time.Clock()
+
+    width, height = 640, 480
+    screen = pygame.display.set_mode((width, height))
+
+    # Game loop.
+    while True:
+        screen.fill((0, 0, 0))
+
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+
+        # Update.
+
+        # Draw.
+
+        pygame.display.flip()
+        fpsClock.tick(fps)
+
+
+if __name__=='__main__':
+    run()
+
