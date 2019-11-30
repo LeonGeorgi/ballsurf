897796766ecffe42e0e6b64e3f84aefeb98db95a
Lars Westermann <lars-westermann@live.de>
2019-11-30 22:39:31 +0100

    Change ball sprites to images

--- src/sprites/ball.py
+++ src/sprites/ball.py
@@ -1,13 +1,12 @@
 import uuid
 from abc import abstractmethod, ABC
-from typing import Tuple
 
-import pygame
 import pygame.gfxdraw
 
 from const import Const
 from context import Context
 from gamerect import GameRect
+from image import CachedImage
 from sprite import Sprite, Sprites, Type
 
 
@@ -18,18 +17,23 @@ class Ball(Sprite, ABC):
             self.__x = 2 * Const.game_height
         else:
             self.__x = x * Const.game_height
+
+        self.image = CachedImage(self.filename())
+        self.diameter = self.image.get_width() * Const.pixel_size
+
         self.id = uuid.uuid4()
 
     def update(self, context: Context, sprites: Sprites):
         self.__x -= context.x_delta
 
     def render(self, surface: pygame.Surface, size_factor: float):
-        pygame.gfxdraw.filled_ellipse(surface, *self.box.to_ellipse_data(size_factor), self.color())
-        pygame.gfxdraw.aaellipse(surface, *self.box.to_ellipse_data(size_factor), self.color())
+        d = int(self.diameter * size_factor)
+        img = self.image.scale(d, d)
+        surface.blit(img, (self.__x * size_factor, (Const.game_height - self.diameter) * size_factor))
 
     @property
     def box(self) -> GameRect:
-        return GameRect(self.__x, Const.game_height - self.diameter(), self.diameter(), self.diameter())
+        return GameRect(self.__x, Const.game_height - self.diameter, self.diameter, self.diameter)
 
     def type(self) -> Type:
         return Type.BALL
@@ -38,10 +42,7 @@ class Ball(Sprite, ABC):
         return self.__x <= -1
 
     @abstractmethod
-    def diameter(self) -> float:
-        """
-        :return: the diameter of the ball in meter
-        """
+    def filename(self) -> str:
         pass
 
     @abstractmethod
@@ -49,8 +50,4 @@ class Ball(Sprite, ABC):
         """
         :return: by how much the velocity of the player is multiplied when he hits the ball
         """
-        pass
-
-    @abstractmethod
-    def color(self) -> Tuple[int, int, int]:
         pass
--- src/sprites/balls/balls.py
+++ src/sprites/balls/balls.py
@@ -1,58 +1,42 @@
-from typing import Tuple
-
 from sprites.ball import Ball
 
 
 class BouncyBall(Ball):
-    def diameter(self) -> float:
-        return 0.7
+
+    def filename(self) -> str:
+        return "../res/img/ball_regular.png"
 
     def bounciness(self) -> float:
         return 1.6
 
-    def color(self) -> Tuple[int, int, int]:
-        return 235, 68, 199
-
 
 class DeadBall(Ball):
-    def diameter(self) -> float:
-        return 0.6
+    def filename(self) -> str:
+        return "../res/img/ball_regular.png"
 
     def bounciness(self) -> float:
         return 0.35
 
-    def color(self) -> Tuple[int, int, int]:
-        return 150, 150, 150
-
 
 class LargeBall(Ball):
-    def diameter(self) -> float:
-        return 1.1
+    def filename(self) -> str:
+        return "../res/img/ball_regular.png"
 
     def bounciness(self) -> float:
         return 0.75
 
-    def color(self) -> Tuple[int, int, int]:
-        return 189, 111, 209
-
 
 class RegularBall(Ball):
-    def diameter(self) -> float:
-        return 0.8
+    def filename(self) -> str:
+        return "../res/img/ball_regular.png"
 
     def bounciness(self) -> float:
         return 1
 
-    def color(self) -> Tuple[int, int, int]:
-        return 26, 160, 237
-
 
 class SmallBall(Ball):
-    def diameter(self) -> float:
-        return 0.5
+    def filename(self) -> str:
+        return "../res/img/ball_regular.png"
 
     def bounciness(self) -> float:
         return 1.2
-
-    def color(self) -> Tuple[int, int, int]:
-        return 224, 124, 43
