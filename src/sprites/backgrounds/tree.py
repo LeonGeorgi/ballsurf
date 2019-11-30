575ef1e0f67a3ddf3360c1d335b7f5f46d9b6f80
Lars Westermann <lars-westermann@live.de>
2019-11-30 14:31:10 +0100

    Improve tree sizes

--- src/sprites/backgrounds/tree.py
+++ src/sprites/backgrounds/tree.py
@@ -9,10 +9,12 @@ from sprites.background import Background
 class Tree(Background):
 
     def __init__(self, x: Optional[float] = None):
-        h = 2.0
+        top = Const.game_height * (0.25 * random.random() + 0.65)
+        h = (top / Const.game_height - 0.65) * 1.5 / 0.3 + 1
+        top -= h
         super().__init__(
             f"../res/img/tree.png",
-            Const.game_height * (0.25 * random.random() + 0.65) - h,
+            top,
             h,
             0.5,
             x
